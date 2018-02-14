8ef35be2ab1d71e9b66801c3dd2d848b074868ec
Ebba <Ebba@130.229.172.225>
2018-02-14 11:23:11 +0100

    Updated all methods, run still needs to be written

--- src/session/User.java
+++ src/session/User.java
@@ -16,45 +16,54 @@ public class User implements Runnable {
 	private BufferedReader in;
 	
 	public void User(Socket mySocket) {
-		
+		Thread th = new Thread(this);
+		connection = mySocket;
+		th.start();
 	}
 	
 	public void User(String hostAddress, int port) {
+		Thread th = new Thread(this);
 		try {
             connection = new Socket(hostAddress, port);
             out = new PrintWriter(connection.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
                                         connection.getInputStream()));
+            th.start();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host.\n" + e);
-            System.exit(1);
         } catch (IOException e) {
         	System.err.println(e);
-        	System.exit(1);
         }
-		
-		run();
 	}
 	
 	public void run() {
 		String userInput;
 		while ((userInput = in.readLine()) != null) {
-		    writeLine(userInput);
+		    
+			//XML parsning 
+			
+			//meddelande: firemessage event
+			//Request: fireRequest event
 		}
-		
 	}
 	
-	public void writeLine(String message) {
-		out.println(message);
+	/**
+	 * sends XML formatted message over the internet.
+	 * @param xmlFormattedMessage
+	 */
+	public void writeLine(String xmlFormattedMessage) {
+		out.println(xmlFormattedMessage);
 	}
-	
+	/**
+	 * Closes the socket.
+	 * Send a disconnect message with writeLine before calling this method.
+	 */
 	public void disconnect() {
-		out.println("disconnect");
+		connection.close();
 	}
 	
 	public void addObserver(ConnectionObserver o) {
 		observers.add(o);
-		
 	}
 	
 	public void fireMessageEvent(Message m) {
@@ -69,27 +78,38 @@ public class User implements Runnable {
 		}
 	}
 	
-	public void sendKeyRequest(CryptoType c, String message) {
-		Request keyRequest = new OutgoingKeyRequest(60, message, this, c);
-		myRequests.add(keyRequest);
+	public void addRequest(Request r) {
+		myRequests.add(r);
 	}
 	
-	public void sendFileRequest(File file, CryptoType c, String message) {
-		Request fileRequest = new OutgoingFileRequest(60, message, this, file, c);
-		myRequests.add(fileRequest);
-	}
+//	public void sendKeyRequest(CryptoType c, String message) {
+//		Request keyRequest = new OutgoingKeyRequest(Request.DEFAULT_LIFETIME, message, this, c);
+//		myRequests.add(keyRequest);
+//	}
+	
+//	public void sendFileRequest(File file, CryptoType c, String message) {
+//		Request fileRequest = new OutgoingFileRequest(60, message, this, file, c);
+//		myRequests.add(fileRequest);
+//	}
 	
 	public void setActiveCrypto(CryptoType c) {
-		
+		switch (c) {
+			case CAESAR:
+				activeCrypto = CaesarCrypto;
+			case AES:
+				activeCrypto = AESCrypto;
+		}
 	}
 	
-	public void sendConnectRequest(String message) {
-		Request connectRequest = new OutgoingConnectionRequest(60, message, this);
-		myRequests.add(connectRequest);
-	}
+//	public void sendConnectRequest(String message) {
+//		Request connectRequest = new OutgoingConnectionRequest(60, message, this);
+//		myRequests.add(connectRequest);
+//	}
 	
 	public void fireUserNotificationEvent(Message message) {
-		
+		for (ConnectionObserver o : observers) {
+			o.newNotification(message);
+		}
 	}
 
 }
