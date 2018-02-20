51bfd61ebe18c7c4d243bee18d273139ad21ba35
thaggus <thogust@kth.se>
2018-02-20 11:58:26 +0100

    Did work on Request and added IncomingConnectionRequest as internal class in Server

---
+++ src/session/ConnectionRequest.java
@@ -0,0 +1,11 @@
+package session;
+
+/**
+ * Class that groups Incoming and Outgoing connection requests into a 
+ * common type. This class adds no functionality of its own: it is empty.
+ * @author thaggus
+ *
+ */
+public abstract class ConnectionRequest extends Request {
+	/* Class intentionally left blank */
+}
--- src/session/Server.java
+++ src/session/Server.java
@@ -3,6 +3,8 @@ package session;
 import java.awt.Color;
 import java.net.ServerSocket;
 
+import javax.swing.text.Element;
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Server extends SessionController implements Runnable {
@@ -19,5 +21,44 @@ public class Server extends SessionController implements Runnable {
 		throw new NotImplementedException();
 	}
 	
-	
+	/**
+	 * Request class for incoming connections.
+	 * @author thaggus
+	 */
+	public class IncomingConnectionRequest extends ConnectionRequest {
+
+		private User myUser;
+		private String myMessage;
+		private Element myElement;
+		
+		public IncomingConnectionRequest(User userIn, String messageIn,
+											Element htmlElementIn) {
+			myUser = userIn;
+			myMessage = messageIn;
+			myElement = htmlElementIn;
+		}
+		
+		@Override
+		public void accept(String message) {
+			//TODO: send the proper XML message to the user
+			connectedUsers.add(myUser);
+			notifyObservers();
+			//done
+		}
+
+		@Override
+		public void deny(String message) {
+			//TODO: send the proper XML message to the user
+			myUser.disconnect();
+			//done
+		}
+
+		@Override
+		protected void timeOut() {
+			//TODO: modify the HTML Element
+			//TODO: write a message in the chat log
+			deny("Request timed out.");
+			//done
+		}
+	}
 }
