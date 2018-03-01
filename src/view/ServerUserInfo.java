c23dc52d432a611c7128ec305b5ee2979b9c6745
thaggus <thogust@kth.se>
2018-03-01 12:12:42 +0100

    Forgot some files in last push

--- src/view/ServerUserInfo.java
+++ src/view/ServerUserInfo.java
@@ -1,5 +1,17 @@
 package view;
 
+import session.SessionController;
+
 public class ServerUserInfo extends ClientUserInfo {
+
+	public ServerUserInfo(SessionController sc) {
+		super(sc);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected JPanel newRow(String username) {
+		
+	}
 
 }
